8ea583ba5704d5b460c5d226b74aa3a6de674273
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-02-24 15:39:04 +0900

    view

---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+
+import { Task } from '../Types';
+
+
+const TaskInput: React.FC= () => {
+    const [inputTitle, setInputTitle] = useState('')
+    
+    const hndleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputTitle(e.target.value) 
+    }
+    return (
+        <div className='input-form'>
+            <div className='inner'>
+                <input
+                    type='text'
+                    className='input'
+                    value={inputTitle}
+                    onChange={hndleInputChange}
+                />
+                <button className='btn is-primary'>追加</button>
+            </div>
+        </div>
+  );
+}
+
+export default TaskInput;
