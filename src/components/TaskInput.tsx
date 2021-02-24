95f934bc1ab952caa5c8315dcde0c971187b3fdf
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-02-24 17:01:14 +0900

    Add:checkbox

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -2,13 +2,32 @@ import React,{useState} from 'react';
 
 import { Task } from '../Types';
 
+type Props = {
+    tasks: Task[]
+    setTasks:React.Dispatch<React.SetStateAction<Task[]>>
+}
 
-const TaskInput: React.FC= () => {
+const TaskInput: React.FC<Props>= ({tasks,setTasks}) => {
     const [inputTitle, setInputTitle] = useState('')
-    
+    const [count, setCount] = useState(tasks.length + 1)
+
     const hndleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputTitle(e.target.value) 
     }
+
+    const handleSubmit = ()=> {
+        setCount(count + 1)
+        
+        const newTask: Task = {
+            id: count,
+            title: inputTitle,
+            done:false
+        }
+
+        setTasks([newTask, ...tasks])
+        setInputTitle('')
+}
+
     return (
         <div className='input-form'>
             <div className='inner'>
@@ -18,7 +37,7 @@ const TaskInput: React.FC= () => {
                     value={inputTitle}
                     onChange={hndleInputChange}
                 />
-                <button className='btn is-primary'>追加</button>
+                <button className='btn is-primary' onClick={handleSubmit}>追加</button>
             </div>
         </div>
   );
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,18 +3,27 @@ import React from 'react';
 import { Task } from '../Types';
 
 type Props = {
-    task: Task
+  task: Task
+  handleDone: (task: Task) => void
+  handleDelete:(task:Task) => void
 }
 
-const  TaskItem: React.FC<Props> = ({task}) => {
+const  TaskItem: React.FC<Props> = ({task,handleDelete,handleDone}) => {
     return (
       
-      <li>
+      <li className={task.done ? 'done':''}>
           <label>
-              <input type='checkbox' className='checkbox-input' />
+          <input
+            type='checkbox'
+            className='checkbox-input'
+            onClick={() => handleDone(task)}
+            defaultChecked={task.done}
+              />
               <span className='checkbox-label'>{task.title}</span>
             </label>
-            <button className ='btn is-delete'>削除</button>
+        <button
+          onClick={() => handleDelete(task)}
+          className='btn is-delete'>削除</button>
     </li>
   );
 }
